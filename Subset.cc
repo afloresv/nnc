cc29a0a374e613bc3b8b672768649d20535f00b9
Alejandro Flores V <alejandroflores.af@gmail.com>
2014-11-12 09:53:28 -0430

    bla

--- Subset.cc
+++ Subset.cc
@@ -6,26 +6,36 @@ using namespace std;
 
 class Subset {
 	int n;
+	Dataset &D;
 	vector<bool> is;
 
 	public:
-	Subset() {}
-	Subset(Dataset D) {
-		is = vector<bool>(D.size());
+	Subset (Dataset &_D) : D(_D) {
+		is = vector<bool>(D.size(),false);
 		n = 0;
 	}
-	int size() {
+	int size () {
 		return n;
 	}
-	void set(int i, bool v=true) {
+	void set (int i, bool v=true) {
 		if (is[i] && !v) n--;
 		else if (!is[i] && v) n++;
 		is[i] = v;
 	}
-	void flip(int i) {
+	void flip (int i) {
 		set(i,!is[i]);
 	}
-	bool operator[](const int& i) {
+	bool operator[] (const int &i) {
 		return is[i];
+	}
+	void operator=(const Subset &S) { 
+		n = S.n;
+		is = S.is;
+	}
+	void print () {
+		printf("Subset with %d points\n",n);
+		for (int i=0,_n=0 ; i<is.size() ; i++)
+			if (is[i]) printf((_n++==0 ? "%d" : ",%d"),D[i].ind);
+		printf("\n");
 	}
 };
