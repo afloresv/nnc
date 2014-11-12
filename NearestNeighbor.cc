cc87bcdf36088bbd3056c661f2de775c4f1962c1
Alejandro Flores V <alejandroflores.af@gmail.com>
2014-11-12 09:52:45 -0430

    self match & error calculation

--- NearestNeighbor.cc
+++ NearestNeighbor.cc
@@ -5,16 +5,21 @@
 using namespace std;
 
 class NearestNeighbor : public KDtree {
-
 	public:
-
 	NearestNeighbor (Dataset &_D) : KDtree(_D) {}
-
-	int of(int ind) {
+	int of (int ind) {
 		return search(ind);
 	}
-
-	bool classify(int ind) {
+	bool classify (int ind) {
 		return (D[search(ind)].c == D[ind].c);
+	}
+	void selfmatch (bool sm) {
+		smatch = sm;
+	}
+	int wrong () {
+		int w=0;
+		for (int i=D.size()-1 ; i>=0 ; i--)
+			if (!classify(i)) w++;
+		return w;
 	}
 };
