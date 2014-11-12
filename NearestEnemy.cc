5d7bcd99722d164f52517a4e3a1e15772ee61386
Alejandro Flores V <alejandroflores.af@gmail.com>
2014-11-12 09:52:15 -0430

    NE distance order

--- NearestEnemy.cc
+++ NearestEnemy.cc
@@ -1,22 +1,28 @@
 #include <iostream>
 #include <cstdio>
 #include <vector>
+#include <algorithm>
+#include <utility>
 
 using namespace std;
 
 class NearestEnemy : public KDtree {
 
 	public:
-
 	NearestEnemy (Dataset &_D) : KDtree(_D) {
 		enemy = true;
 	}
-
-	int of(int ind) {
+	int of (int ind) {
 		return search(ind);
 	}
-
-	double distance(int ind) {
-		return D.distance(search(ind));
+	double distance (int ind) {
+		return D.distance(ind,search(ind));
+	}
+	vector<pair<double,int> > order () {
+		vector<pair<double,int> > ne;
+		for (int i=D.size()-1 ; i>=0 ; i--)
+			ne.push_back(make_pair(distance(i),i));
+		sort(ne.begin(),ne.end());
+		return ne;
 	}
 };
